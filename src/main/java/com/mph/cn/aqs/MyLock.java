6523fe24bcc8fbf9024d6621383f4ccafa02c8c4
miaoph <miaoph@52iuh.com>
2019-09-17 17:41:16 +0800

    实现 自己的aqs 锁

---
+++ src/main/java/com/mph/cn/aqs/MyLock.java
@@ -0,0 +1,74 @@
+package com.mph.cn.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * aqs 实现自己的锁
+ */
+public class MyLock implements Lock {
+    //state 表示获取到锁 state=1 获取到了锁，state=0，表示这个锁当前没有线程拿到
+    public static class Sync extends AbstractQueuedSynchronizer {
+        protected boolean isHeldExclusively() {
+            //判断是否占用锁
+            return getState() == 1;
+        }
+
+        //获取独占锁
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        //释放独占锁
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new UnsupportedOperationException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    private final static Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.tryRelease(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
---
+++ src/main/java/com/mph/cn/aqs/TestMyLock.java
@@ -0,0 +1,46 @@
+package com.mph.cn.aqs;
+
+import com.mph.cn.utils.SleepTools;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TestMyLock {
+    public void test() {
+        final Lock lock =
+//                new ReentrantLock();
+                new MyLock();
+
+        class Worker extends Thread {
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        SleepTools.currentSleep(1);
+                        System.out.println(Thread.currentThread().getName());
+                        SleepTools.currentSleep(1);
+                    } finally {
+                        lock.unlock();
+                    }
+                    SleepTools.currentSleep(2);
+                }
+            }
+        }
+        // 启动10个子线程
+        for (int i = 0; i < 10; i++) {
+            Worker w = new Worker();
+            w.setDaemon(true);
+            w.start();
+        }
+        // 主线程每隔1秒换行
+        for (int i = 0; i < 10; i++) {
+            SleepTools.currentSleep(10);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        TestMyLock testMyLock = new TestMyLock();
+        testMyLock.test();
+    }
+}
